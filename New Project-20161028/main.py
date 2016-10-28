abb5ced950f23e940055a0479bd774bc10c782f7
flopezmontero <falm81427@gmail.com>
2016-10-28 14:17:54 -0600

    Update main.py

--- New Project-20161028/main.py
+++ New Project-20161028/main.py
@@ -7,13 +7,23 @@
 #Francis Alexander Lopez Montero
 #28/10/2016
 
+
+#Lista de Trabajo
+#1. Carga de los archivos de Excel
+#2. Almacenamiento de la configuración (Posicion de las celdas correspondientes a mi trabajo)
+#3. localizar rango de celdas de trabajo
+#4.  Catalogar errores (N/A, Passed, Failed, Failed Automatic)
+#5. Escritura a hoja de excel
+
 import struct
 import string
 #import openpyx1
 import os
 import shutil
+import shelve
+import pyperclip
 
-def load_exc(a):
+def load_exc(a):#Carga de las hojas de excel
     
     direc=os.getcwd();
     #excpath=os.path.join(direc,"example.xlsx");
@@ -40,19 +50,73 @@ def load_exc(a):
             
             
         
+def config (oper,typ):
+    oper=oper+typ+3;
+    typ=oper*2;
+    print "suma"
+    return oper,typ;
     
 
+def save (lista_pos,lista_fila,lista_col):
+    list_file = shelve.open("lista")
+    list_file['lista_pos'] = lista_pos
+    list_file['lista_fila'] =lista_fila
+    list_file['lista_col'] = lista_col
+    list_file.close()
+    
+def load():
+    lista_pos=[];
+    lista_fila=[];
+    lista_col=[];
+    if (os.path.isfile("lista")== True):
+        list_file = shelve.open("lista")
+        lista_pos = list_file['lista_pos']
+        lista_fila = list_file['lista_fila']
+        lista_col = list_file['lista_col']
+        list_file.close()
+    return lista_pos,lista_fila,lista_col
+#########################################################################################################################
+#En prueba
+########################################################################################################################
+
+def find_and_list(wb,lista):
+    name=raw_input("Digite nombre: ")
+    n=0;
+    sheet = wb.get_sheet_by_name('Hoja1')
+    for i in range(2, sheet.max_row+1):
+        if sheet.cell(row=i, column=1).value == name :
+            lista.append(sheet.cell(row=i, column=1).coordinate);
+            n=n+1;
+            
+    if lista == []:
+        print ("No se encontraron similitudes")
+    else:
+        print ("Se encontraron y cargaron %d similitudes",n)
+        
+    return lista
+    
+    
+def show_test (wb,lista_pos,lista_fila): #muestra el primer elemento de la lista con sus detalles
+    sheet = wb.get_sheet_by_name('Hoja1')
+    for i in range(1,sheet.max_column):
+        print("%s: %s",sheet.cell(row=1, column=i).value, sheet.cell(row=lista_fila[0], column=i))
 
 
 def main():
+	
+    lista_pos=[];
+    lista_fila=[];
+    lista_col=[];
+    #lista.append()
     oper=0
-    typ=0#Formato simple=0;Formato doble=1
-    n=1024;#Cantidad de datos
+    typ=0
+    n=1024
     a=True
     wb=load_exc(a)
+    lista_pos,lista_fila,lista_col = load ();
     
     menu = {}
-    menu['1']="Cambiar Operacion a realizar" 
+    menu['1']="Almacenar rango de trabajo" 
     menu['2']="Formato simple/Formato doble"
     menu['3']="Crear valores aleatorios en hexadecimal"
     menu['4']="(Postsimulacion) Calcular y graficar error"
@@ -68,10 +132,19 @@ def main():
 			print entry, menu[entry]
 	    selection=raw_input("Please Select:")
 	    if selection =='1':
-		    print
-	    elif selection == '5': 
-			break
+	        oper,typ=config(oper,typ);
+	        print
+	    elif selection == '2':
+	        #lista.append(oper)
+	        pyperclip.copy("ola k ase")
+	        print
+	    elif selection == '3':
+	        for i in lista:
+	            print(i)
+	        print
+	    elif selection == '5':
+	        save(lista_pos,lista_fila,lista_col)
+	        break
 	    print
 	    wb.close();
-	########Simulacion en Verilog(vivado o otra herramienta)
 main();
