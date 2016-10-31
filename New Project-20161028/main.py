55606ec338ad80a3794498514f7e720832919251
flopezmontero <falm81427@gmail.com>
2016-10-31 17:30:50 -0600

    Update main.py

--- New Project-20161028/main.py
+++ New Project-20161028/main.py
@@ -24,136 +24,130 @@ import shelve
 #import openpyx1 #pip install openpyxl
 
 def load_exc(a):#Carga de las hojas de excel
-    
-    direc=os.getcwd();
-    #excpath=os.path.join(direc,"example.xlsx");
-    excpath=os.path.join(direc,"Newfile.txt");
-    #excpath=os.path.join(direc,"example.xlsx");
-    exccopypath=os.path.join(direc,"NewfileW.txt");
-    
-    if (os.path.isfile(excpath)== True):
-        print "Archivo excel existente \n";
-        if (os.path.isfile(exccopypath)== True):
-            print "Copia de excel para trabajo existente";
-        else:
-            print "No hay copia de excel para trabajo existente \n";
-            print "Creando copia \n";
-            shutil.copy2(excpath, exccopypath);
-            
-        #wb=openpyx1.load_workbook("exampleW.xls");
-        wb=open(exccopypath);
-    else:
-        print "No hay archivo excel para leer";
-        a=False;
-        
-    return wb;    
+	
+	direc=os.getcwd();
+	#excpath=os.path.join(direc,"example.xlsx");
+	excpath=os.path.join(direc,"Newfile.txt");
+	#excpath=os.path.join(direc,"example.xlsx");
+	exccopypath=os.path.join(direc,"NewfileW.txt");
+
+	if (os.path.isfile(excpath)== True):
+		print "Archivo excel existente \n";
+		if (os.path.isfile(exccopypath)== True):
+			print "Copia de excel para trabajo existente";
+		else:
+			print "No hay copia de excel para trabajo existente \n";
+			print "Creando copia \n";
+			shutil.copy2(excpath, exccopypath);
+
+		#wb=openpyx1.load_workbook("exampleW.xls");
+		wb=open(exccopypath);
+	else:
+		print "No hay archivo excel para leer";
+		a=False;
+	return wb;    
             
             
         
 def config (oper,typ):
-    oper=oper+typ+3;
-    typ=oper*2;
-    print "suma"
-    return oper,typ;
+	oper=oper+typ+3;
+	typ=oper*2;
+	print "suma"
+	return oper,typ;
     
 
 def save (lista_pos,lista_fila,lista_col):
-    list_file = shelve.open("lista")
-    list_file['lista_pos'] = lista_pos
-    list_file['lista_fila'] =lista_fila
-    list_file['lista_col'] = lista_col
-    list_file.close()
-    
+	list_file = shelve.open("lista")
+	list_file['lista_pos'] = lista_pos
+	list_file['lista_fila'] =lista_fila
+	list_file['lista_col'] = lista_col
+	list_file.close()
+	
 def load():
-    lista_pos=[];
-    lista_fila=[];
-    lista_col=[];
-    if (os.path.isfile("lista")== True):
-        list_file = shelve.open("lista")
-        lista_pos = list_file['lista_pos']
-        lista_fila = list_file['lista_fila']
-        lista_col = list_file['lista_col']
-        list_file.close()
-    return lista_pos,lista_fila,lista_col
+	lista_pos=[];
+	lista_fila=[];
+	lista_col=[];
+	if (os.path.isfile("lista")== True):
+		list_file = shelve.open("lista")
+		lista_pos = list_file['lista_pos']
+		lista_fila = list_file['lista_fila']
+		lista_col = list_file['lista_col']
+		list_file.close()
+	return lista_pos,lista_fila,lista_col
 #########################################################################################################################
 #En prueba
 ########################################################################################################################
 
 def find_and_list(wb,lista):
-    name=raw_input("Digite nombre: ")
-    n=0;
-    sheet = wb.get_sheet_by_name('Hoja1')
-    for i in range(2, sheet.max_row+1):
-        if sheet.cell(row=i, column=1).value == name :
-            lista.append(sheet.cell(row=i, column=1).coordinate);
-            n=n+1;
-            
-    if lista == []:
-        print ("No se encontraron similitudes")
-    else:
-        print ("Se encontraron y cargaron %d similitudes",n)
-        
-    return lista
-    
-    
+	name=raw_input("Digite nombre: ")
+	n=0;
+	sheet = wb.get_sheet_by_name('Hoja1')
+	for i in range(2, sheet.max_row+1):
+		if sheet.cell(row=i, column=1).value == name :
+			lista.append(sheet.cell(row=i, column=1).coordinate);
+			n=n+1;
+	if lista == []:
+		print ("No se encontraron similitudes")
+	else:
+		print ("Se encontraron y cargaron %d similitudes",n)
+        return lista
+
 def show_test (wb,lista_pos,lista_fila): #muestra el primer elemento de la lista con sus detalles
-    sheet = wb.get_sheet_by_name('Hoja1')
-    if (lista_pos==[]):
-        print("No hay elementos cargados o ya ha finalizado su trabajo ;)")
-    else:
-        for i in range(1,sheet.max_column):
-            print("%s: %s",sheet.cell(row=1, column=i).value, sheet.cell(row=lista_fila[0], column=i).value)
-            if(sheet.cell(row(row=1, column=i).value=="Autocase")):
-                pyperclip.copy(sheet.cell(row=lista_fila[0], column=i).value)
-        print("###########################")
-    return 0
+	sheet = wb.get_sheet_by_name('Hoja1')
+	if (lista_pos==[]):
+		print("No hay elementos cargados o ya ha finalizado su trabajo ;)")
+	else:
+		for i in range(1,sheet.max_column):
+			print("%s: %s",sheet.cell(row=1, column=i).value, sheet.cell(row=lista_fila[0], column=i).value)
+			if(sheet.cell(row(row=1, column=i).value=="Autocase")):
+				pyperclip.copy(sheet.cell(row=lista_fila[0], column=i).value)
+		print("###########################")
+	return 0
     
 #def modify_testcase(wb,lista_pos,lista_fila): #añadir la informacion extra
 
 
 def main():
 	
-    lista_pos=[];
-    lista_fila=[];
-    lista_col=[];
-    
-    oper=0
-    typ=0
-    n=1024
-    a=True
-    wb=load_exc(a)
-    lista_pos,lista_fila,lista_col = load ();
-    
-    menu = {}
-    menu['1']="Almacenar rango de trabajo" 
-    menu['2']="Obtener info de un testcase"
-    menu['3']="Crear valores aleatorios en hexadecimal"
-    menu['4']="(Postsimulacion) Calcular y graficar error"
-    menu['5']="Exit"
-    while a:
-        
-	    options=menu.keys()
-	    options.sort()
-	    print "Operacion %d" %oper;
-	    print "Formato %d" %typ;
-	    print "Cantidad %d" %n;
-	    for entry in options: 
+	lista_pos=[];
+	lista_fila=[];
+	lista_col=[];
+	oper=0;
+	typ=0
+	n=1024
+	a=True
+	wb=load_exc(a)
+	lista_pos,lista_fila,lista_col = load ();
+	
+	menu = {}
+	menu['1']="Almacenar rango de trabajo" 
+	menu['2']="Obtener info de un testcase"
+	menu['3']="Crear valores aleatorios en hexadecimal"
+	menu['4']="(Postsimulacion) Calcular y graficar error"
+	menu['5']="Exit"
+	while a:
+		options=menu.keys()
+		options.sort()
+		print "Operacion %d" %oper;
+		print "Formato %d" %typ;
+		print "Cantidad %d" %n;
+		for entry in options: 
 			print entry, menu[entry]
-	    selection=raw_input("Please Select:")
-	    if selection =='1':
-	        oper,typ=config(oper,typ);
-	        print
-	    elif selection == '2':
-	        #lista.append(oper)
-	        #pyperclip.copy("ola k ase")
-	        print
-	    elif selection == '3':
-	        for i in lista:
-	            print(i)
-	        print
-	    elif selection == '5':
-	        save(lista_pos,lista_fila,lista_col)
-	        break
-	    print
-	    wb.close();
+		selection=raw_input("Please Select:")
+		if selection =='1':
+			oper,typ=config(oper,typ);
+			print
+		elif selection == '2':
+		#lista.append(oper)
+		#pyperclip.copy("ola k ase")
+			print
+		elif selection == '3':
+			for i in lista:
+				print(i)
+			print
+		elif selection == '5':
+			save(lista_pos,lista_fila,lista_col)
+			break
+	print
+	wb.close();
 main();
